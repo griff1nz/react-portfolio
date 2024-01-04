9d8644b066b076730712a0ddf941ce52f0836616
Zion Griffin <defendingmymaininbox@gmail.com>
2024-01-04 09:31:09 -0500

    Made the navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
 import { render } from 'react-dom';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter, Switch } from 'react-router-dom';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
 import Home from './pages/Home';
+import MainNav from './components/layout/MainNav';
 
 function App() {
   return (
     <div>
+      <MainNav />
       <Routes>
-
+        <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/projects' element={<Projects />} />
         <Route path='/contact' element={<Contact />} />
-        <Route path='/' element={<Home />} />
       </Routes>
     </div>
   );
---
+++ src/components/layout/MainNav.js
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+import classes from './MainNav.css';
+function MainNav() {
+    return <header>
+        <nav className="backdrop-blur-lg">
+            <div className="w-11/12 flex flex-wrap items-center justify-between mx-auto p-4">
+                <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Zion Griffin's Portfolio</span>
+                </Link>
+                <Link to='/'>
+                    <img id='logo' src='../../logo.svg'></img>
+                </Link>
+                <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+                    <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+                        <li>
+                            <a to="#" className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</a>
+                        </li>
+                        <li>
+                            <Link to="/about" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About Me</Link>
+                        </li>
+                        <li>
+                            <Link to="/projects" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projects</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    </header>
+}
+export default MainNav;
